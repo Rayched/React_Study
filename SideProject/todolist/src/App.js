992e1295c87b74441a33ba572971aab55ea401fc
Rayched <ny2401@naver.com>
2024-01-10 13:07:36 +0900

    2024.01.10 : To do List Part I 완료 및 전개 구문 메모 추가

--- SideProject/todolist/src/App.js
+++ SideProject/todolist/src/App.js
@@ -14,12 +14,43 @@ function App() {
     } 
     setToDo("");
     setToDos(function (currentArray){ return [ToDo, ...currentArray]});
+    /**
+     * [...Array] == 전개 구문 spread Syntax
+     * 
+     * App이 처음 실행될 때 currentArray에는 인자로 빈 배열이 들어간다.
+     * (ToDos의 초기 값 == [] 빈 배열)
+     * 일정 입력 form에서 일정을 입력 (React 공부)
+     *'추가'버튼을 클릭하면 입력한 값이 ToDo에 담긴다.
+     * 
+     * state 'ToDos'의 값을 변경하는 함수 setToDos의 인자인 콜백 함수는
+     * ToDo와 currentArray가 담긴 배열을 return한다.
+     * (이때 currentArray에 담긴 값은 [빈 배열]이므로)
+     * ('[ToDo]' == ["React 공부"] 배열을 return한다.)
+     * 그리고 setToDos 함수에 의해 return된 배열이
+     * ToDos가 참조하는 state에 저장된다.
+     * 
+     * 그리고 이 상태에서 새로운 일정을 입력하고 추가를 하면 (JS 공부)
+     * 방금 전 입력한 일정이 ToDo에 담기고
+     * setToDos 함수의 인자인 콜백 함수가 실행된다.
+     * 그리고 방금 전 입력한 값과 인자 currentArray에 전달된 값을
+     * 하나의 배열로 만들어서 return한다.
+     * 이때 currentArray에 전달된 값은 ToDos의 값과 동일한
+     * 
+     * 이전에 ToDos state에 저장된 값인 '["React 공부"] 배열이
+     * 해당 매개변수 currentArray에 인자로 전달된다.
+     * 
+     * 그리고 setToDos의 인자인 콜백 함수은 [ToDo, ToDos]
+     * ["JS 공부", "React 공부"] 배열을 return하고
+     * 해당 값은 다시 ToDos에 저장된다. (state에 저장)
+     */
   };
 
   console.log(ToDos);
 
   return (
     <div>
+      <h3>To Do List</h3>
+      <h4>현재 일정 개수 : {ToDos.length}</h4>
       <form onSubmit={onSubmit}>
         <input 
           onChange={onChange}
@@ -36,6 +67,9 @@ function App() {
 export default App;
 
 /**
+ * React - To do List Part I
+ * 
+ * 2024.01.09 월요일
  * 1. To Do List 기본 코드 작성
  * '추가' 버튼을 클릭하면 내가 입력한 일정이 화면에 나오도록 logic 작성
  * (form submit (input, button))
@@ -56,6 +90,13 @@ export default App;
  *  - state를 직접적으로 수정 불가하니
  *    기존 배열 함수 array.push()를 통해 일정을 ToDos 배열에 담을 수 없다.
  *  - ToDos를 수정하는 setToDos 함수를 통해서, 일정을 배열에 추가 가능
- *    단, Array function은 사용 불가하므로 아래 함수 추가해서 해결함.
- *    
+ *    단, Array function은 사용 불가하므로 Spread Syntax 문법을 활용해서
+ *    현재 추가한 일정 및 이전 일정을 배열로 보여줌
+ * 
+ * 4. 지금까지 추가했던 일정을 보여주는 Element 추가 (2024.01.10 화요일)
+ */
+
+/**
+ * React - To do List Part II
+ * 
  */
